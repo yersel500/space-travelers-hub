3263aa179e4bdc1f565adb582f7e2f88536a6173
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-04-05 16:20:26 -0300

    Add styling

--- src/components/rockets/Rocket.js
+++ src/components/rockets/Rocket.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 const Rocket = ({
   id, image, name, description,
 }) => (
-  <div id={id}>
-    <img src={image} alt="Rocket" />
-    <h2>{name}</h2>
-    <p>{description}</p>
-    <button type="button">Reserve Rocket</button>
+  <div className="rocket-container" id={id}>
+    <img className="rocket-img" src={image} alt="Rocket" />
+    <div>
+      <h2 className="rocket-name">{name}</h2>
+      <p className="rocket-description">{description}</p>
+      <button className="rocket-btn" type="button">Reserve Rocket</button>
+    </div>
   </div>
 );
 
