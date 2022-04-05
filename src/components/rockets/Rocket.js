82df4183c4b01105f63a31ee09585181817145b5
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-04-05 18:58:52 -0300

    Add actions to change reservation

--- src/components/rockets/Rocket.js
+++ src/components/rockets/Rocket.js
@@ -1,23 +1,43 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeReservation, reserveRocket } from '../../redux/rockets/RocketsReducer';
 
 const Rocket = ({
-  id, image, name, description,
-}) => (
-  <div className="rocket-container" id={id}>
-    <img className="rocket-img" src={image} alt="Rocket" />
-    <div>
-      <h2 className="rocket-name">{name}</h2>
-      <p className="rocket-description">{description}</p>
-      <button className="rocket-btn" type="button">Reserve Rocket</button>
+  id, image, name, description, reserved,
+}) => {
+  const dispatch = useDispatch();
+
+  const reserve = () => {
+    dispatch(reserveRocket(id));
+  };
+
+  const cancelReservation = () => {
+    dispatch(removeReservation(id));
+  };
+
+  return (
+    <div className="rocket-container" id={id}>
+      <img className="rocket-img" src={image} alt="Rocket" />
+      <div>
+        <h2 className="rocket-name">{name}</h2>
+        <p className="rocket-description">{description}</p>
+        {reserved === true && (
+          <button className="rocket-cancel-btn" onClick={cancelReservation} type="button">Cancel Reservation</button>
+        )}
+        {reserved === false && (
+          <button className="rocket-reserve-btn" onClick={reserve} type="button">Reserve Rocket</button>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Rocket.propTypes = {
   image: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 
 export default Rocket;
--- src/redux/rockets/RocketsReducer.js
+++ src/redux/rockets/RocketsReducer.js
@@ -1,4 +1,6 @@
 const GET_ROCKETS = 'rockets/RocketsReducer/GET_ROCKETS';
+const RESERVE_ROCKET = 'rockets/RocketsReducer/RESERVE_ROCKET';
+const REMOVE_RESERVATION = 'rockets/RocketsReducer/REMOVE_RESERVATION';
 
 const rocketsURL = 'https://api.spacexdata.com/v3/rockets';
 
@@ -8,6 +10,20 @@ export const rocketsReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_ROCKETS:
       return action.payload;
+    case RESERVE_ROCKET:
+      return state.map((rocket) => {
+        if (rocket.id !== action.id) {
+          return rocket;
+        }
+        return { ...rocket, reserved: true };
+      });
+    case REMOVE_RESERVATION:
+      return state.map((rocket) => {
+        if (rocket.id !== action.id) {
+          return rocket;
+        }
+        return { ...rocket, reserved: false };
+      });
     default:
       return state;
   }
@@ -16,6 +32,16 @@ export const rocketsReducer = (state = initialState, action) => {
 export const rocketsData = (payload) => ({
   type: GET_ROCKETS,
   payload,
+});
+
+export const reserveRocket = (id) => ({
+  type: RESERVE_ROCKET,
+  id,
+});
+
+export const removeReservation = (id) => ({
+  type: REMOVE_RESERVATION,
+  id,
 });
 
 export const getRockets = async () => {
@@ -28,6 +54,7 @@ export const getRockets = async () => {
       name: rocket.rocket_name,
       image: rocket.flickr_images[0],
       description: rocket.description,
+      reserved: false,
     });
   });
   return rockets;
